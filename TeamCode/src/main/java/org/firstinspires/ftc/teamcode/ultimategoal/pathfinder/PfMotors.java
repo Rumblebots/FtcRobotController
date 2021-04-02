07c7aafdf4ffe7f117b9b6a54bc6f8b6e1d7336d
colin robertson <wobblyyyy@gmail.com>
2021-04-01 20:26:32 -0400

    update pathfinder, try testing, didn't work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ultimategoal/pathfinder/PfMotors.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ultimategoal/pathfinder/PfMotors.java
@@ -5,34 +5,26 @@ import me.wobblyyyy.pathfinder.robot.Motor;
 import me.wobblyyyy.pathfinder.robot.MotorBuilder;
 
 public class PfMotors {
-    private static final boolean FR_INVERT = false;
-    private static final boolean FL_INVERT = false;
-    private static final boolean BR_INVERT = false;
-    private static final boolean BL_INVERT = false;
-
     public final Motor frontRight;
     public final Motor frontLeft;
     public final Motor backRight;
     public final Motor backLeft;
 
-    /**
-     * TODO (so someone sees this later)
-     * this method uses suppliers and consumers which don't work with any api
-     * under 24, if we need to downgrade APIs this will throw a hissy fit
-     * we can accomplish the same thing by implementing the {@link Motor}
-     * interface in a separate class file, but this is certainly preferable
-     */
     public PfMotors(DcMotor fr,
                     DcMotor fl,
                     DcMotor br,
-                    DcMotor bl) {
+                    DcMotor bl,
+                    boolean invertFr,
+                    boolean invertFl,
+                    boolean invertBr,
+                    boolean invertBl) {
         frontRight = MotorBuilder.buildFrom(
-                fr::setPower, fr::getPower, FR_INVERT);
+                fr::setPower, fr::getPower, invertFr);
         frontLeft = MotorBuilder.buildFrom(
-                fl::setPower, fl::getPower, FL_INVERT);
+                fl::setPower, fl::getPower, invertFl);
         backRight = MotorBuilder.buildFrom(
-                br::setPower, br::getPower, BR_INVERT);
+                br::setPower, br::getPower, invertBr);
         backLeft = MotorBuilder.buildFrom(
-                bl::setPower, bl::getPower, BL_INVERT);
+                bl::setPower, bl::getPower, invertBl);
     }
 }
