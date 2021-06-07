d607ef81272e7bc1a0e397f756e5beb018e63a57
colin <wobblyyyy@gmail.com>
2021-06-07 19:21:20 -0400

    Change PointSnapshot types. Use integers instead of doubles to save space. Doubles have very long string representations, which makes JSON exports incredibly long. Integers are much shorter and don't sacrifice too much - we shouldn't really need to render the robot's position with a degree of accuracy such that rounding to the nearest inch will cause any harm. The odometry system isn't mechanically accurate enough to warrant using doubles, so ints should suffice.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/recorder/PointSnapshot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/recorder/PointSnapshot.java
@@ -1,9 +1,9 @@
 package org.firstinspires.ftc.teamcode.recorder;
 
 public class PointSnapshot {
-    private double x;
-    private double y;
-    private double z;
+    private int x;
+    private int y;
+    private int z;
 
     public PointSnapshot() {
         this(0, 0, 0);
@@ -12,32 +12,32 @@ public class PointSnapshot {
     public PointSnapshot(double x,
                          double y,
                          double z) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
+        this.x = (int) Math.round(x);
+        this.y = (int) Math.round(y);
+        this.z = (int) Math.round(z);
     }
 
-    public double getX() {
+    public int getX() {
         return this.x;
     }
 
-    public double getY() {
-        return this.x;
+    public int getY() {
+        return this.y;
     }
 
-    public double getZ() {
-        return this.x;
+    public int getZ() {
+        return this.z;
     }
 
     public void setX(double x) {
-        this.x = x;
+        this.x = (int) Math.round(x);
     }
 
     public void setY(double y) {
-        this.y = y;
+        this.y = (int) Math.round(y);
     }
 
     public void setZ(double z) {
-        this.z = z;
+        this.z = (int) Math.round(z);
     }
 }
